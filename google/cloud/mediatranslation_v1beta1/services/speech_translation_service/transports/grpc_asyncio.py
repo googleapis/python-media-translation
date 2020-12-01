419c61210234bcd7ae2b12fedb8fd5d66be10504
yoshi-automation <yoshi-automation@google.com>
2020-12-01 08:07:33 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- google/cloud/mediatranslation_v1beta1/services/speech_translation_service/transports/grpc_asyncio.py
+++ google/cloud/mediatranslation_v1beta1/services/speech_translation_service/transports/grpc_asyncio.py
@@ -16,23 +16,25 @@
 #
 
 import warnings
-from typing import Callable, Dict, Optional, Sequence, Tuple
+from typing import Awaitable, Callable, Dict, Optional, Sequence, Tuple
 
-from google.api_core import grpc_helpers  # type: ignore
 from google.api_core import gapic_v1  # type: ignore
+from google.api_core import grpc_helpers_async  # type: ignore
 from google import auth  # type: ignore
 from google.auth import credentials  # type: ignore
 from google.auth.transport.grpc import SslCredentials  # type: ignore
 
 import grpc  # type: ignore
+from grpc.experimental import aio  # type: ignore
 
 from google.cloud.mediatranslation_v1beta1.types import media_translation
 
 from .base import SpeechTranslationServiceTransport, DEFAULT_CLIENT_INFO
+from .grpc import SpeechTranslationServiceGrpcTransport
 
 
-class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
-    """gRPC backend transport for SpeechTranslationService.
+class SpeechTranslationServiceGrpcAsyncIOTransport(SpeechTranslationServiceTransport):
+    """gRPC AsyncIO backend transport for SpeechTranslationService.
 
     Provides translation from/to media types.
 
@@ -44,20 +46,62 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
     top of HTTP/2); the ``grpcio`` package must be installed.
     """
 
-    _stubs: Dict[str, Callable]
+    _grpc_channel: aio.Channel
+    _stubs: Dict[str, Callable] = {}
+
+    @classmethod
+    def create_channel(
+        cls,
+        host: str = "mediatranslation.googleapis.com",
+        credentials: credentials.Credentials = None,
+        credentials_file: Optional[str] = None,
+        scopes: Optional[Sequence[str]] = None,
+        quota_project_id: Optional[str] = None,
+        **kwargs,
+    ) -> aio.Channel:
+        """Create and return a gRPC AsyncIO channel object.
+        Args:
+            address (Optional[str]): The host for the channel to use.
+            credentials (Optional[~.Credentials]): The
+                authorization credentials to attach to requests. These
+                credentials identify this application to the service. If
+                none are specified, the client will attempt to ascertain
+                the credentials from the environment.
+            credentials_file (Optional[str]): A file with credentials that can
+                be loaded with :func:`google.auth.load_credentials_from_file`.
+                This argument is ignored if ``channel`` is provided.
+            scopes (Optional[Sequence[str]]): A optional list of scopes needed for this
+                service. These are only used when credentials are not specified and
+                are passed to :func:`google.auth.default`.
+            quota_project_id (Optional[str]): An optional project to use for billing
+                and quota.
+            kwargs (Optional[dict]): Keyword arguments, which are passed to the
+                channel creation.
+        Returns:
+            aio.Channel: A gRPC AsyncIO channel object.
+        """
+        scopes = scopes or cls.AUTH_SCOPES
+        return grpc_helpers_async.create_channel(
+            host,
+            credentials=credentials,
+            credentials_file=credentials_file,
+            scopes=scopes,
+            quota_project_id=quota_project_id,
+            **kwargs,
+        )
 
     def __init__(
         self,
         *,
         host: str = "mediatranslation.googleapis.com",
         credentials: credentials.Credentials = None,
-        credentials_file: str = None,
-        scopes: Sequence[str] = None,
-        channel: grpc.Channel = None,
+        credentials_file: Optional[str] = None,
+        scopes: Optional[Sequence[str]] = None,
+        channel: aio.Channel = None,
         api_mtls_endpoint: str = None,
         client_cert_source: Callable[[], Tuple[bytes, bytes]] = None,
         ssl_channel_credentials: grpc.ChannelCredentials = None,
-        quota_project_id: Optional[str] = None,
+        quota_project_id=None,
         client_info: gapic_v1.client_info.ClientInfo = DEFAULT_CLIENT_INFO,
     ) -> None:
         """Instantiate the transport.
@@ -73,9 +117,10 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
             credentials_file (Optional[str]): A file with credentials that can
                 be loaded with :func:`google.auth.load_credentials_from_file`.
                 This argument is ignored if ``channel`` is provided.
-            scopes (Optional(Sequence[str])): A list of scopes. This argument is
-                ignored if ``channel`` is provided.
-            channel (Optional[grpc.Channel]): A ``Channel`` instance through
+            scopes (Optional[Sequence[str]]): A optional list of scopes needed for this
+                service. These are only used when credentials are not specified and
+                are passed to :func:`google.auth.default`.
+            channel (Optional[aio.Channel]): A ``Channel`` instance through
                 which to make calls.
             api_mtls_endpoint (Optional[str]): Deprecated. The mutual TLS endpoint.
                 If provided, it overrides the ``host`` argument and tries to create
@@ -89,14 +134,14 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
                 for grpc channel. It is ignored if ``channel`` is provided.
             quota_project_id (Optional[str]): An optional project to use for billing
                 and quota.
-            client_info (google.api_core.gapic_v1.client_info.ClientInfo):
-                The client info used to send a user-agent string along with
-                API requests. If ``None``, then default info will be used.
-                Generally, you only need to set this if you're developing
+            client_info (google.api_core.gapic_v1.client_info.ClientInfo):	
+                The client info used to send a user-agent string along with	
+                API requests. If ``None``, then default info will be used.	
+                Generally, you only need to set this if you're developing	
                 your own client library.
 
         Raises:
-          google.auth.exceptions.MutualTLSChannelError: If mutual TLS transport
+            google.auth.exceptions.MutualTlsChannelError: If mutual TLS transport
               creation failed for any reason.
           google.api_core.exceptions.DuplicateCredentialArgs: If both ``credentials``
               and ``credentials_file`` are passed.
@@ -166,8 +211,6 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
                 quota_project_id=quota_project_id,
             )
 
-        self._stubs = {}  # type: Dict[str, Callable]
-
         # Run the base constructor.
         super().__init__(
             host=host,
@@ -178,55 +221,16 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
             client_info=client_info,
         )
 
-    @classmethod
-    def create_channel(
-        cls,
-        host: str = "mediatranslation.googleapis.com",
-        credentials: credentials.Credentials = None,
-        credentials_file: str = None,
-        scopes: Optional[Sequence[str]] = None,
-        quota_project_id: Optional[str] = None,
-        **kwargs,
-    ) -> grpc.Channel:
-        """Create and return a gRPC channel object.
-        Args:
-            address (Optional[str]): The host for the channel to use.
-            credentials (Optional[~.Credentials]): The
-                authorization credentials to attach to requests. These
-                credentials identify this application to the service. If
-                none are specified, the client will attempt to ascertain
-                the credentials from the environment.
-            credentials_file (Optional[str]): A file with credentials that can
-                be loaded with :func:`google.auth.load_credentials_from_file`.
-                This argument is mutually exclusive with credentials.
-            scopes (Optional[Sequence[str]]): A optional list of scopes needed for this
-                service. These are only used when credentials are not specified and
-                are passed to :func:`google.auth.default`.
-            quota_project_id (Optional[str]): An optional project to use for billing
-                and quota.
-            kwargs (Optional[dict]): Keyword arguments, which are passed to the
-                channel creation.
-        Returns:
-            grpc.Channel: A gRPC channel object.
-
-        Raises:
-            google.api_core.exceptions.DuplicateCredentialArgs: If both ``credentials``
-              and ``credentials_file`` are passed.
-        """
-        scopes = scopes or cls.AUTH_SCOPES
-        return grpc_helpers.create_channel(
-            host,
-            credentials=credentials,
-            credentials_file=credentials_file,
-            scopes=scopes,
-            quota_project_id=quota_project_id,
-            **kwargs,
-        )
+        self._stubs = {}
 
     @property
-    def grpc_channel(self) -> grpc.Channel:
-        """Return the channel designed to connect to this service.
+    def grpc_channel(self) -> aio.Channel:
+        """Create the channel designed to connect to this service.
+
+        This property caches on the instance; repeated calls return
+        the same channel.
         """
+        # Return the channel from cache.
         return self._grpc_channel
 
     @property
@@ -234,7 +238,7 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
         self
     ) -> Callable[
         [media_translation.StreamingTranslateSpeechRequest],
-        media_translation.StreamingTranslateSpeechResponse,
+        Awaitable[media_translation.StreamingTranslateSpeechResponse],
     ]:
         r"""Return a callable for the streaming translate speech method over gRPC.
 
@@ -244,7 +248,7 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
 
         Returns:
             Callable[[~.StreamingTranslateSpeechRequest],
-                    ~.StreamingTranslateSpeechResponse]:
+                    Awaitable[~.StreamingTranslateSpeechResponse]]:
                 A function that, when called, will call the underlying RPC
                 on the server.
         """
@@ -261,4 +265,4 @@ class SpeechTranslationServiceGrpcTransport(SpeechTranslationServiceTransport):
         return self._stubs["streaming_translate_speech"]
 
 
-__all__ = ("SpeechTranslationServiceGrpcTransport",)
+__all__ = ("SpeechTranslationServiceGrpcAsyncIOTransport",)
