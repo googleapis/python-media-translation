1913b34f4a990203405abdd7b3260aa68a8aeb78
Eric Schmidt <erschmid@google.com>
2021-06-17 16:27:08 -0700

    fix: lint

--- samples/translate_from_file.py
+++ samples/translate_from_file.py
@@ -22,33 +22,35 @@ Example usage:
 from google.cloud import mediatranslation
 
 
-def translate_from_file(file_path='path/to/your/file'):
+def translate_from_file(file_path="path/to/your/file"):
     client = mediatranslation.SpeechTranslationServiceClient()
 
     # The `sample_rate_hertz` field is not required for FLAC and WAV (Linear16)
     # encoded data. Other audio encodings must provide the sampling rate.
     audio_config = mediatranslation.TranslateSpeechConfig(
-        audio_encoding='linear16',
-        source_language_code='en-US',
-        target_language_code='fr-FR')
+        audio_encoding="linear16",
+        source_language_code="en-US",
+        target_language_code="fr-FR",
+    )
 
     streaming_config = mediatranslation.StreamingTranslateSpeechConfig(
-        audio_config=audio_config, single_utterance=True)
+        audio_config=audio_config, single_utterance=True
+    )
 
     def request_generator(config, audio_file_path):
 
         # The first request contains the configuration.
         # Note that audio_content is explicitly set to None.
-        yield mediatranslation.StreamingTranslateSpeechRequest(
-            streaming_config=config)
+        yield mediatranslation.StreamingTranslateSpeechRequest(streaming_config=config)
 
-        with open(audio_file_path, 'rb') as audio:
+        with open(audio_file_path, "rb") as audio:
             while True:
                 chunk = audio.read(4096)
                 if not chunk:
                     break
-                yield mediatranslation.StreamingTranslateSpeechRequest(audio_content=chunk)
-
+                yield mediatranslation.StreamingTranslateSpeechRequest(
+                    audio_content=chunk
+                )
 
     requests = request_generator(streaming_config, file_path)
     responses = client.streaming_translate_speech(requests)
@@ -57,13 +59,13 @@ def translate_from_file(file_path='path/to/your/file'):
         # Once the transcription settles, the response contains the
         # is_final result. The other results will be for subsequent portions of
         # the audio.
-        print(f'Response: {response}')
+        print(f"Response: {response}")
         result = response.result
         translation = result.text_translation_result.translation
 
         if result.text_translation_result.is_final:
-            print(u'\nFinal translation: {0}'.format(translation))
+            print("\nFinal translation: {0}".format(translation))
             break
 
-        print(u'\nPartial translation: {0}'.format(translation))
+        print("\nPartial translation: {0}".format(translation))
     # [END media_translation_translate_from_file]
